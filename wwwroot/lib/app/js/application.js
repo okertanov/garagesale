396821b547b568943fd55fe948dfc3cb5bddd67c
okertanov <okertanov@gmail.com>
2012-06-05 17:37:51 +0300

    images form input 7

--- wwwroot/lib/app/js/application.js
+++ wwwroot/lib/app/js/application.js
@@ -472,7 +472,7 @@ exports.CategoryModel = Backbone.Model.extend(
     },
     url: function()
     {
-        return '/api/category' + (this.isNew() ? '' : '/' + this.get('_id'));
+        return '/api/category' + ( typeof this.get('_id') !== 'undefined' ? '/' + this.get('_id') : '' );
     },
     initialize: function()
     {
@@ -504,7 +504,7 @@ exports.ItemModel = Backbone.Model.extend(
     },
     url: function()
     {
-        return '/api/items' + '/' + this.get('category') + (this.isNew() ? '' : '/' + this.get('_id'));
+        return '/api/items' + '/' + this.get('category') + ( typeof this.get('_id') !== 'undefined' ? '/' + this.get('_id') : '' );
     },
     initialize: function()
     {
